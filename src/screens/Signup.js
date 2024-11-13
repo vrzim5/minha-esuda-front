0dcedd5d7e39a4bef280aa4b84d408c77f24f20a
Vinicius Regis <152446386+vrzim5@users.noreply.github.com>
2024-11-13 20:47:56 -0300

    Atualizado a tela Signup.js
    - Adicionado o Input Nome
    
    - Adicionado o Design acordado, seguindo todas as funcionalidade da tela de login

--- src/screens/Signup.js
+++ src/screens/Signup.js
@@ -3,63 +3,102 @@ import {
   View,
   Text,
   TextInput,
-  Button,
-  StyleSheet,
   TouchableOpacity,
+  StyleSheet,
   Alert,
+  ActivityIndicator,
+  Image
 } from "react-native";
 import { registerUser } from "../services/api";
 
 const Signup = ({ navigation }) => {
+  const [nome, setNome] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const handleSignup = async () => {
-    if (email && password && confirmPassword) {
+    if (nome && email && password && confirmPassword) {
       if (password === confirmPassword) {
-        const response = await registerUser(email, password);
+        setLoading(true);
+        const response = await registerUser(nome, email, password);
+        setLoading(false);
+
         if (response.success) {
-          Alert.alert("Signup Successful", "You can now log in.");
+          Alert.alert("Cadastro realizado com sucesso", "Agora você pode fazer login.");
           navigation.navigate("Login");
         } else {
-          Alert.alert("Signup Failed", response.message);
+          Alert.alert("Falha no cadastro", response.message);
         }
       } else {
-        Alert.alert("Error", "Passwords do not match.");
+        Alert.alert("Erro", "As senhas não coincidem.");
       }
     } else {
-      Alert.alert("Error", "Please fill in all fields.");
+      Alert.alert("Erro", "Por favor, preencha todos os campos.");
     }
   };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Sign Up</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        value={email}
-        onChangeText={setEmail}
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        value={password}
-        onChangeText={setPassword}
-        secureTextEntry
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Confirm Password"
-        value={confirmPassword}
-        onChangeText={setConfirmPassword}
-        secureTextEntry
-      />
-      <Button title="Sign Up" onPress={handleSignup} />
-      <TouchableOpacity onPress={() => navigation.navigate("Login")}>
-        <Text style={styles.linkText}>Already have an account? Login</Text>
-      </TouchableOpacity>
+      {/* Imagem sobre o banner */}
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../../assets/LogoEsuda.png')} // Substitua com o caminho da sua imagem
+          style={styles.overlayImage}
+        />
+      </View>
+
+      {/* Formulário de cadastro */}
+      <View style={styles.formContainer}>
+        <Text style={styles.title}>Criar Conta</Text>
+
+        <TextInput
+          style={styles.input}
+          placeholder="Nome"
+          value={nome}
+          onChangeText={setNome}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Ex: 00000000@esuda.edu.br"
+          value={email}
+          onChangeText={setEmail}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Senha"
+          value={password}
+          onChangeText={setPassword}
+          secureTextEntry
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Confirmar Senha"
+          value={confirmPassword}
+          onChangeText={setConfirmPassword}
+          secureTextEntry
+        />
+
+        {/* Botão de cadastro */}
+        <TouchableOpacity
+          style={styles.button}
+          onPress={handleSignup}
+          disabled={loading}
+        >
+          <Text style={styles.buttonText}>{loading ? "Carregando..." : "Cadastrar"}</Text>
+        </TouchableOpacity>
+
+        {loading && <ActivityIndicator size="large" color="#1e90ff" />}
+
+        {/* Link para redirecionar para a tela de login */}
+        <View style={styles.loginLinkContainer}>
+          <Text style={styles.Text}>Já tem uma conta? </Text>
+          <TouchableOpacity onPress={() => navigation.navigate("Login")}>
+            <Text style={styles.linkText}>Logar-se</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 };
@@ -67,15 +106,43 @@ const Signup = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    padding: 16,
-    backgroundColor: "#f5f5f5",
+    backgroundColor: "#DB914A", // Cor de fundo
+    justifyContent: "flex-start", // Começar o conteúdo do topo
+  },
+  logoContainer: {
+    flex: 1,  // Faz com que a logo ocupe o espaço disponível
+    justifyContent: "center",  // Alinha verticalmente ao centro
+    alignItems: "center",  // Alinha horizontalmente ao centro
+    position: 'relative',
+  },
+  overlayImage: {
+    marginTop: "10%",
+    width: 200, // Ajuste o tamanho da imagem conforme necessário
+    height: 200,
+    resizeMode: 'contain', // A imagem vai se ajustar dentro do espaço disponível sem distorcer
+  },
+  formContainer: {
+    flex: 2, // Permite que o formulário ocupe o restante do espaço
+    marginTop: -50,  // Ajusta a posição do formulário para um alinhamento mais adequado
+    width: "100%",
+    top: 75,
+    backgroundColor: "#fff",
+    borderTopRightRadius: 110,
+    padding: 50,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 5, // Sombra para Android
   },
   title: {
-    fontSize: 24,
+    fontSize: 50,
     fontWeight: "bold",
     marginBottom: 24,
+    textAlign: 'center',
   },
   input: {
     width: "100%",
@@ -85,10 +152,35 @@ const styles = StyleSheet.create({
     borderColor: "#ccc",
     borderRadius: 8,
     backgroundColor: "#fff",
+    color: "#808080",
+  },
+  button: {
+    backgroundColor: "#61B375",
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    borderRadius: 8,
+    marginTop: 20,
+    marginHorizontal: 50,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+  loginLinkContainer: {
+    flexDirection: 'row', // Alinha os itens horizontalmente
+    justifyContent: 'center', // Centraliza o conteúdo horizontalmente
+    marginTop: 16,
   },
   linkText: {
-    color: "#1e90ff",
-    marginTop: 16,
+    color: "#DB914A", // Cor do texto "Faça login"
+    fontWeight: "bold",
+  },
+  Text: {
+    color: "#000", // Cor do texto "Já tem uma conta?"
+    fontWeight: "bold",
   },
 });
 
